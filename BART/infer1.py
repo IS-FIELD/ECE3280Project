07967377ac9d1d0fbfc3b8c0af0bace67f709251
IsF <123090391@link.cuhk.edu.cn>
2025-04-24 14:33:02 +0800

    加入注释，修改infer1

--- BART/infer1.py
+++ BART/infer1.py
@@ -10,6 +10,31 @@ inputs = tokenizer(
     text, return_tensors="pt", truncation=True, padding="max_length", max_length=256
 )
 outputs = model(**inputs)
-probs = outputs.logits.softmax(dim=-1)
-pred_label_id = probs.argmax(dim=-1).item()
-print(pred_label_id)
+probs = outputs.logits.softmax(dim=-1)  # 这里就是所有以及标签的置信度
+
+
+fst_label_map = {
+    "0": "Applied computing",
+    "1": "Computer systems organization",
+    "2": "Computing methodologies",
+    "3": "General and reference",
+    "4": "Hardware",
+    "5": "Human-centered computing",
+    "6": "Information systems",
+    "7": "Mathematics of computing",
+    "8": "Networks",
+    "9": "Security and privacy",
+    "10": "Theory of computation",
+}
+
+# 取前5个id
+probs = probs.squeeze(0)  # shape: [num_labels]
+
+# 获取前5个概率最大的索引
+top_5_indices = probs.topk(5).indices.tolist()  # 返回前5个最大值的索引
+top_5_ids = [str(i) for i in top_5_indices]
+top_5_labels = [fst_label_map[id] for id in top_5_ids]
+print(f"top_5_labels: {top_5_labels}")
+
+most_prob_id = probs.argmax(dim=-1).item()
+print(f"most probably label: {fst_label_map[str(most_prob_id)]}")
--- BART/modelpush.py
+++ BART/modelpush.py
@@ -1,8 +1,11 @@
 from huggingface_hub import HfApi
 import os
+
 api = HfApi(token=os.getenv("HF_TOKEN"))
+# 先创建仓库（如果已存在不会报错）
+
 api.upload_folder(
-    folder_path="/mnt/data4/luyiheng/BARTfinetune/lr_5e-05/checkpoint-195",
-    repo_id="IsField/AcBART",
+    folder_path="/mnt/data4/luyiheng/AcBART2/lr_5e-05/checkpoint-195",
+    repo_id="IsField/AcBART2",
     repo_type="model",
 )
